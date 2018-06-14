4c9f3b673b0d06c8e89c568b67057fcfeabf2ddd
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-14 06:15:37 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array,string)
+  array.push(string)
+end
+def using_unshift(array,string)
+  array.unshift(string)
+end
+def using_pop(array)
+  el = array.pop 
+  el
+end
+def pop_with_args(array)
+  new_arr = []
+  2.times do
+    new_arr.push(array.pop) 
+  end
+  new_arr.reverse
+end
+def using_shift(array)
+  arg = array.shift 
+  arg
+end
+def shift_with_args(array)
